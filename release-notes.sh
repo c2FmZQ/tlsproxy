110a855bd3eeb6084b2eee0fde371152d6d8e16e
Robin Thellend <rthellend@gmail.com>
2023-12-09 18:47:05 -0800

    Show tags in release notes

--- release-notes.sh
+++ release-notes.sh
@@ -3,12 +3,18 @@
 
 cd $(dirname "$0")
 version=$(echo $1 | sed -re 's/[+].*$//' -e 's/[.]/[.]/g')
+tags=$(echo $1 | sed -re 's/^([^+]*)([+](.*))?$/\3/' -e 's/[+]/,/g') 
 
 awk '
   /^## / { if (on) exit 0 }
   /^## '"${version}"'$/ { on=1 }
   { if (on) print }
 ' < CHANGELOG.md
+
+if [[ -n "${tags}" ]]; then
+  echo 'Built with: `-tags '"${tags}"'`'
+  echo
+fi
 
 prev=$(grep "^## v" CHANGELOG.md | grep -E '^## '"${version}"'$' -A1 | tail -n 1 | cut -c4-)
 if [[ "${prev}" != "" && "${prev}" != "$1" ]]; then
