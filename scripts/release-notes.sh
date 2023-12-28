ef268d915292f045ac18336fb57221e210c94bd0
Robin Thellend <rthellend@gmail.com>
2023-12-28 11:42:18 -0800

    Experiment with automation
    Automating go updates.

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 # Display the release notes for the version passed as command line argument.
 
-cd $(dirname "$0")
 version=$(echo $1 | sed -re 's/[+].*$//' -e 's/[.]/[.]/g')
 tags=$(echo $1 | sed -re 's/^([^+]*)([+](.*))?$/\3/' -e 's/[+]/,/g') 
 
---
+++ scripts/update-go-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Check the lastest GO version and update the github workflows.
+
+latest=$(curl 'https://go.dev/dl/?mode=json' | jq -r '.[].stable = true | .[].version' | head -n 1)
+echo "Latest GO version is ${latest}"
+if [[ "${latest}" =~ ^go ]]; then
+  version="${latest#go}"
+  for f in .github/workflows/*.yml; do
+    sed -i -re "s/GOVERSION: .*/GOVERSION: '>=${version}'/" $f
+  done
+fi
