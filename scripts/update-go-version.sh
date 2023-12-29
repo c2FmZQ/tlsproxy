06ce2dd94fd308c749f576883b297a5e9293a0f3
Robin Thellend <rthellend@gmail.com>
2023-12-28 16:40:20 -0800

    Update dockerfile and changelog

--- scripts/update-go-version.sh
+++ scripts/update-go-version.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 #
-# Check the lastest GO version and update the github workflows.
-
-echo '```'
+# Check the latest GO version and update the github workflows.
 
 latest=$(curl -s 'https://go.dev/dl/?mode=json' | jq -r '.[].stable = true | .[].version' | head -n 1)
-echo "GO version: ${latest}"
+version="${latest#go}"
 if [[ "${latest}" =~ ^go ]]; then
-  version="${latest#go}"
+  sed -i -re "s/^FROM golang:.*/FROM golang:${version}-alpine3.18 AS build/" Dockerfile
   for f in .github/workflows/*.yml; do
     sed -i -re "s/GOVERSION: .*/GOVERSION: '>=${version}'/" $f
   done
 fi
 
-go get -u ./... 2>&1 | grep upgrade
+deps=$(go get -u ./... 2>&1 | grep upgrade)
 
-echo '```'
-
+sed -n '1,2p' < CHANGELOG.md > CHANGELOG.md-new
+if [[ -n $(git status -s Dockerfile) ]]; then
+  echo "* update to go '${version}" | tee -a CHANGELOG.md-new
+fi
+if [[ -n "${deps}" ]]; then
+  echo "${deps}" | sed -re 's/^/* /g' | tee -a CHANGELOG.md-new
+fi
+sed -n '3,$p' < CHANGELOG.md >> CHANGELOG.md-new
+mv CHANGELOG.md-new CHANGELOG.md
 exit 0
