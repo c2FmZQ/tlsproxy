bec447735404b0666e0f2eeae3e96f9e6eec008a
Robin Thellend <rthellend@rthellend.com>
2024-04-02 19:09:38 -0700

    Sign release binaries and docker images (#99)
    ### Description
    
    Sign release binaries and docker images in the release workflow.
    
    ### Type of change
    
    * [ ] New feature
    * [x] Feature improvement
    * [ ] Bug fix
    * [ ] Documentation
    * [ ] Cleanup / refactoring
    * [ ] Other (please explain)

--- scripts/build-release-binaries.sh
+++ scripts/build-release-binaries.sh
@@ -4,6 +4,13 @@ mkdir -p bin
 
 go generate ./...
 
+sign() {
+  local name="$1"
+  if [[ -n "${GPG_PASSPHRASE}" ]]; then
+    gpg --batch --pinentry-mode loopback --yes --passphrase "${GPG_PASSPHRASE}" --detach-sig --local-user c2FmZQ-bot --output "${name}.sig" "${name}"
+  fi
+}
+
 export CGO_ENABLED=0
 export GOARM=7
 flag="-ldflags=-extldflags=-static -s -w -X main.Version=${GITHUB_REF_NAME:-dev}"
@@ -11,20 +18,24 @@ tags="$(echo "${GITHUB_REF_NAME:-dev}" | sed -re 's/^v[^+]+[+]?//' -e 's/[.]/,/g
 # tlsproxy
 for os in darwin linux; do
   for arch in amd64 arm64 arm; do
-    echo "Building tlsproxy for ${os}-${arch}"
-    GOOS="${os}" GOARCH="${arch}" go build -a -trimpath "${flag}" -tags "${tags}" -o "bin/tlsproxy-${os}-${arch}" .
+    basename="bin/tlsproxy-${os}-${arch}"
+    echo "Building ${basename}"
+    GOOS="${os}" GOARCH="${arch}" go build -a -trimpath "${flag}" -tags "${tags}" -o "${basename}" .
     if [[ $? == 0 ]]; then
-      sha256sum "bin/tlsproxy-${os}-${arch}" | cut -d " " -f1 > "bin/tlsproxy-${os}-${arch}.sha256"
+      sha256sum "${basename}" | cut -d " " -f1 > "${basename}.sha256"
+      sign "${basename}"
     fi
   done
 done
 # tlsclient
 for os in android darwin linux; do
   for arch in amd64 arm64 arm; do
-    echo "Building tlsclient for ${os}-${arch}"
-    GOOS="${os}" GOARCH="${arch}" go build -a -trimpath "${flag}" -tags "${tags}" -o "bin/tlsclient-${os}-${arch}" ./tlsclient
+    basename="bin/tlsclient-${os}-${arch}"
+    echo "Building ${basename}"
+    GOOS="${os}" GOARCH="${arch}" go build -a -trimpath "${flag}" -tags "${tags}" -o "${basename}" ./tlsclient
     if [[ $? == 0 ]]; then
-      sha256sum "bin/tlsclient-${os}-${arch}" | cut -d " " -f1 > "bin/tlsclient-${os}-${arch}.sha256"
+      sha256sum "${basename}" | cut -d " " -f1 > "${basename}.sha256"
+      sign "${basename}"
     fi
   done
 done
