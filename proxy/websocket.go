30083c010b76fa735e5908509bf527f0568710df
Robin Thellend <rthellend@gmail.com>
2024-11-30 12:57:48 -0800

    add upgrade annotation

--- proxy/websocket.go
+++ proxy/websocket.go
@@ -62,6 +62,8 @@ func (p *Proxy) webSocketHandler(cfg WebSocketConfig) http.Handler {
 		wc := netw.NewConn(out)
 		wc.SetAnnotation(startTimeKey, time.Now())
 		if conn, ok := req.Context().Value(connCtxKey).(anyConn); ok {
+			annotatedConn(conn).SetAnnotation(httpUpgradeKey, "websocket")
+			wc.SetAnnotation(httpUpgradeKey, "websocket")
 			wc.SetAnnotation(serverNameKey, connServerName(conn))
 			wc.SetAnnotation(modeKey, connMode(conn))
 		}
