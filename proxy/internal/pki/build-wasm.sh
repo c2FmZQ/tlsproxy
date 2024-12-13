95f151551c3f62c69c1f9af4d37dedc7cb1f45f1
Robin Thellend <rthellend@gmail.com>
2024-12-13 11:31:48 -0800

    Minor change for go1.24

--- proxy/internal/pki/build-wasm.sh
+++ proxy/internal/pki/build-wasm.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 
 (cd clientwasm && GOOS=js GOARCH=wasm go build -ldflags="-extldflags=-s -w" -o ../pki.wasm .)
 
-cp $(go env GOROOT)/misc/wasm/wasm_exec.js .
+cp $(go env GOROOT)/*/wasm/wasm_exec.js .
 bzip2 -c9 < pki.wasm > pki.wasm.bz2
 ls -l pki.wasm.bz2 wasm_exec.js
